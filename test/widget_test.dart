cf9e3d1844a2b8e4d8380222def349397e439a2f
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2022-11-21 05:55:13 +0330

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:landina_coupon/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LandinaCoupon());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
