29da9de23c68948135e58d82fda0e7d8c2e50e88
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2022-12-03 11:05:07 +0330

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:landina_coupon/ui/pages/categories/categories.dart';
