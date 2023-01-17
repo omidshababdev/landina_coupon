c3d9f0aa5ad6e57124d134c2d24b9924cf3cc82e
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-01-17 08:50:54 +0330

    first commit

--- lib/ui/pages/account/profile/analytics/view.dart
+++ lib/ui/pages/account/profile/analytics/view.dart
@@ -62,66 +62,19 @@ LineChartData mainData() {
       show: false,
     ),
     gridData: FlGridData(
-        show: true,
-        horizontalInterval: 1.6,
-        getDrawingHorizontalLine: (value) {
-          return FlLine(
-            dashArray: const [3, 3],
-            color: const Color(0xff37434d).withOpacity(0.2),
-            strokeWidth: 2,
-          );
-        },
-        drawVerticalLine: false),
+      show: true,
+      horizontalInterval: 1.6,
+      getDrawingHorizontalLine: (value) {
+        return FlLine(
+          dashArray: const [3, 3],
+          // color: const Color(0xff37434d).withOpacity(0.2),
+          strokeWidth: 2,
+        );
+      },
+      drawVerticalLine: false,
+    ),
     titlesData: FlTitlesData(
       show: false,
-      //   rightTitles: SideTitles(showTitles: false),
-      //   topTitles: SideTitles(showTitles: false),
-      //   bottomTitles: SideTitles(
-      //     showTitles: true,
-      //     reservedSize: 22,
-      //     interval: 1,
-      //     getTextStyles: (context, value) => const TextStyle(
-      //         color: Color(0xff68737d),
-      //         fontWeight: FontWeight.bold,
-      //         fontSize: 11),
-      //     getTitles: (value) {
-      //       switch (value.toInt()) {
-      //         case 0:
-      //           return 'MAR';
-      //         case 4:
-      //           return 'JUN';
-      //         case 8:
-      //           return 'SEP';
-      //         case 11:
-      //           return 'OCT';
-      //       }
-      //       return '';
-      //     },
-      //     margin: 10,
-      //   ),
-      //   leftTitles: SideTitles(
-      //     showTitles: true,
-      //     interval: 1,
-      //     getTextStyles: (context, value) => const TextStyle(
-      //       color: Color(0xff67727d),
-      //       fontWeight: FontWeight.bold,
-      //       fontSize: 12,
-      //     ),
-
-      //     getTitles: (value) {
-      //       switch (value.toInt()) {
-      //         case 1:
-      //           return '10k';
-      //         case 3:
-      //           return '30k';
-      //         case 5:
-      //           return '50k';
-      //       }
-      //       return '';
-      //     },
-      //     reservedSize: 25,
-      //     margin: 12,
-      //   ),
     ),
     minX: 0,
     maxX: 11,
@@ -138,15 +91,7 @@ LineChartData mainData() {
           FlSpot(9.5, 4),
           FlSpot(11, 5),
         ],
-        // [
-        //   FlSpot(0, 0),
-        //   FlSpot(2.4, 0),
-        //   FlSpot(4.4, 0),
-        //   FlSpot(6.4, 0),
-        //   FlSpot(8, 0),
-        //   FlSpot(9.5, 0),
-        //   FlSpot(11, 0)
-        // ],
+
         isCurved: true,
         // colors: gradientColors,
         barWidth: 2,
@@ -154,13 +99,7 @@ LineChartData mainData() {
           show: false,
         ),
         belowBarData: BarAreaData(
-          show: true,
-          // gradientFrom: Offset(0, 0),
-          // gradientTo: Offset(0, 1),
-          // colors: [
-          //   Color(0xff02d39a).withOpacity(0.1),
-          //   Color(0xff02d39a).withOpacity(0),
-          // ]
+          show: false,
         ),
       ),
     ],
--- lib/ui/pages/coupon/comments/comments.coupon.dart
+++ lib/ui/pages/coupon/comments/comments.coupon.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:landina_coupon/constants/config.dart';
 import 'package:landina_coupon/ui/widgets/appbar/appbar.dart';
 import 'package:landina_coupon/ui/widgets/comment/comment.dart';
 
@@ -33,8 +35,39 @@ class _CommentsPageState extends State<CommentsPage> {
         physics: const BouncingScrollPhysics(
           parent: ClampingScrollPhysics(),
         ),
-        children: const [
-          Comment(),
+        children: [
+          const Comment(),
+          Center(
+            child: Container(
+              constraints: const BoxConstraints(
+                maxWidth: 325,
+                maxHeight: 500,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/svg/not_found.svg",
+                    color:
+                        Config.darkMode != true ? Colors.black : Colors.white,
+                    width: 250,
+                  ),
+                  const SizedBox(height: 25),
+                  Text(
+                    "دیگه نظری اینجا نیست!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color:
+                          Config.darkMode != true ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
