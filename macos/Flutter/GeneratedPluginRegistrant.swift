89aaae33dd8cdb4bc2bf30d502f1310e3f780bd7
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-01-25 18:56:05 +0330

    first commit

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import share_plus
 import url_launcher_macos
 
